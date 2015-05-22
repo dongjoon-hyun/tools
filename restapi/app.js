ca65ed8f26433e1aedbce1b3091add651392ae58
Dongjoon Hyun <dongjoon@apache.org>
2015-05-22 15:42:00 +0900

    update app.js

--- restapi/app.js
+++ restapi/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var child_process = require('child_process');
 var app = express();
 
 app.use(function(req, res, next) {
@@ -17,8 +18,7 @@ app.use(function(req, res, next) {
     }
 });
 
-app.get('/api/v1/', function(req, res) {
-    res.send('\
+var usage = '\
 <html>\
   <table border="1">\
     <tr><td>classify   </td><td>  [Caffe] </td><td>http://localhost:8080/api/v1/classify </td></tr>\
@@ -34,6 +34,17 @@ app.get('/api/v1/', function(req, res) {
     <tr><td>train_mnist</td><td>  [Caffe] </td><td>http://localhost:8080/api/v1/classify </td></tr>\
     <tr><td>word_cloud </td><td>  [R]     </td><td>http://localhost:8080/api/v1/classify </td></tr>\
   </table>\
-</html>');});
+</html>';
+
+app.get('/api/v1/:op?:param', function(req, res) {
+    child = child_process.spawn('/usr/local/bin/fab', ['-f', '/home/tip/tip/cli/fabfile.py', '%(id)s:%(param)s' % req.params ], { stdio: [ 0, 'pipe', 0 ]});
+    child.stdout.on('data',
+        function (data) {
+            res.send(JSON.stringify(data.toString()));
+        }
+    );
+});
+app.get('/api/v1/:op', function(req, res) { res.send(usage); });
+app.get('/api/v1/', function(req, res) { res.send(usage); });
 
 app.listen(process.env.PORT || 8080);
