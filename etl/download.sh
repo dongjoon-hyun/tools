0a56f8981eda7321891799e3af8aabe42f4dea13
Dongjoon Hyun <dongjoon@apache.org>
2015-05-31 19:49:03 +0900

    parameterize script

--- etl/download.sh
+++ etl/download.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-DIR=/home/ubuntu/2015/news/opengraph
-DB=$DIR/joins.db
-SERVER=ipc4
 
-for i in 40
-do
-  SQL='select url,article from news where url like "%='$i'_____"'
-  FILE=$DIR/${i}00000.txt
-  ssh -i ~/.ssh/IP.pem ubuntu@$SERVER "sqlite3 -separator '' $DB '$SQL' > $FILE"
-  scp -i ~/.ssh/IP.pem ubuntu@$SERVER:$FILE ./joins/
-done
+if [ $# -eq 0 ]; then
+        echo 'Command is missing.'
+        exit
+fi
+
+DIR=/home/ubuntu/2015/news/opengraph
+DB=$DIR/$1
+SERVER=$2
+i=$3
+
+SQL='select url,article from news where url like "%='$i'_____"'
+FILE=$DIR/${i}00000.txt
+ssh -i ~/.ssh/IP.pem ubuntu@$SERVER "sqlite3 -separator '' $DB '$SQL' > $FILE"
+scp -i ~/.ssh/IP.pem ubuntu@$SERVER:$FILE ./joins/
