f769df74e208b70e9612a509697ccc6bcea31870
swlsw <soonwoong.lee@gmail.com>
2015-07-20 23:40:06 +0900

    add word2vec_qa & word2vec_draw

--- cli/nlp.py
+++ cli/nlp.py
@@ -352,15 +352,15 @@ EOF''' % locals())
 
 
 @task
-def word2vec_train(inpath, outpath, numpartitions, numiterations):
+def word2vec_train(inpath, outpath, numpartitions, mincount, numiterations):
     '''
-    fab nlp.word2vec_train:/data/sample/sample_hani_kma,/user/hadoop/sample_hani_wordvec,20,1
+    fab nlp.word2vec_train:/data/sample/sample_hani_kma,/user/hadoop/sample_hani_wordvec,20,5,3
     '''
     if not (outpath.startswith('/tmp/') or outpath.startswith('/user/hadoop/')):
         print 'Unauthorized path: %(outpath)s' % locals()
-	return
+        return
     run('hadoop fs -rm -r -f -skipTrash %(outpath)s &> /dev/null' % locals())
-    cmd = '/opt/spark/bin/spark-submit --master spark://50.1.100.98:7077 --class SparkWord2VecTrain --num-executors 120 --driver-memory 4G --executor-memory 4G --conf spark.akka.frameSize=200 /hdfs/user/hadoop/demo/nlp/sparkword2vec_2.10-1.0.jar %(inpath)s %(outpath)s %(numpartitions)s %(numiterations)s 2> /dev/null' % locals()
+    cmd = '/opt/spark/bin/spark-submit --master spark://50.1.100.98:7077 --class SparkWord2VecTrain2 --num-executors 120 --driver-memory 8G --executor-memory 8G --conf spark.akka.frameSize=200 --conf spark.driver.maxResultSize=2g /hdfs/user/hadoop/demo/nlp/sparkword2vec_2.10-1.0.jar %(inpath)s %(outpath)s %(numpartitions)s %(mincount)s %(numiterations)s 2> /dev/null' % locals()
     run(cmd)
     
 
@@ -369,5 +369,154 @@ def word2vec_test(modelpath, queryword, topn):
     '''
     fab nlp.word2vec_test:/data/sample/sample_hani_wordvec,서울,10
     '''
-    cmd = '/opt/spark/bin/spark-submit --master spark://50.1.100.98:7077 --class SparkWord2VecTest --num-executors 120 --driver-memory 4G --executor-memory 4G --conf spark.akka.frameSize=200 /hdfs/user/hadoop/demo/nlp/sparkword2vec_2.10-1.0.jar %(modelpath)s %(queryword)s %(topn)s  2> /dev/null' % locals()
+#     cmd = '/opt/spark/bin/spark-submit --master spark://50.1.100.98:7077 --class SparkWord2VecTest --num-executors 30 --driver-memory 4G --executor-memory 4G --conf spark.akka.frameSize=200 /hdfs/user/hadoop/demo/nlp/sparkword2vec_2.10-1.0.jar %(modelpath)s %(queryword)s %(topn)s 2> /dev/null' % locals()
+#     run(cmd)
+    run('''cat <<EOF > /home/hadoop/demo/nlp.word2vec_test.py
+# -*- encoding: utf-8 -*-
+from numpy import *
+from numpy.linalg import *
+ 
+argmodelfile = '/hdfs' + '%(modelpath)s' + '/vectors.txt'
+argqueryword = '%(queryword)s' + '/NNP'
+argtopn = int('%(topn)s')
+ 
+fread = open(argmodelfile, 'r')
+wordvectors = {}
+for f in fread.readlines():
+    tok = f.split("\t")
+    w = tok[0].strip()
+    v = tok[1].strip().split(" ")
+    if '/N' in w:
+        wordvectors[w] = [double(d) for d in v]
+ 
+words = [wv[0] for wv in wordvectors.items()]
+vectors = [wv[1] for wv in wordvectors.items()]
+ 
+qwordindex = words.index(argqueryword)
+qvector = vectors[qwordindex]
+ 
+a = array(vectors)
+b = array(qvector)
+c = dot(a, b.T)
+c = [e / norm(vectors[i]) / norm(qvector) for i, e in enumerate(c)]
+ 
+synonyms = [(words[i], c[i]) for i in argsort(c)[::-1][1:argtopn + 1] ]
+for s in synonyms:
+    print s[0], s[1]
+EOF''' % locals())
+    cmd = 'python2.7 /home/hadoop/demo/nlp.word2vec_test.py'
+    run(cmd)
+    
+    
+@task
+def word2vec_qa(modelpath, hint1, hint2, hint3, topn):
+    '''
+    fab nlp.word2vec_qa:/data/sample/sample_hani_wordvec,프랑스,파리,태국,5
+    '''
+    run('''cat <<EOF > /home/hadoop/demo/nlp.word2vec_qa.py
+# -*- encoding: utf-8 -*-
+from numpy import *
+from numpy.linalg import *
+ 
+argmodelfile = '/hdfs' + '%(modelpath)s' + '/vectors.txt'
+arghintwords = ['%(hint1)s' + '/NNP', '%(hint2)s' + '/NNP', '%(hint3)s' + '/NNP']
+argtopn = int('%(topn)s')
+ 
+fread = open(argmodelfile, 'r')
+wordvectors = {}
+for f in fread.readlines():
+    tok = f.split("\t")
+    w = tok[0].strip()
+    v = tok[1].strip().split(" ")
+    if '/N' in w:
+        wordvectors[w] = [double(d) for d in v]
+ 
+words = [wv[0] for wv in wordvectors.items()]
+vectors = [wv[1] for wv in wordvectors.items()]
+ 
+vec1 = array(vectors[words.index(arghintwords[0])])
+vec2 = array(vectors[words.index(arghintwords[1])])
+vec3 = array(vectors[words.index(arghintwords[2])])
+
+ansvec = (vec2 - vec1) + vec3
+
+a = array(vectors)
+c = dot(a, ansvec.T)
+c = [e / norm(vectors[i]) / norm(ansvec) for i, e in enumerate(c)]
+
+synonyms = [(words[i], c[i]) for i in argsort(c)[::-1][1:argtopn + 1] ]
+for s in synonyms:
+    print s[0], s[1]
+EOF''' % locals())
+    cmd = 'python2.7 /home/hadoop/demo/nlp.word2vec_qa.py'
+    run(cmd)
+    
+    
+@task
+def word2vec_draw(modelpath, outpath, topn):
+    '''
+    fab nlp.word2vec_draw:/data/sample/sample_hani_wordvec,/tmp/word2vec_draw.png,100
+    '''
+    if not (outpath.startswith('/tmp/') or outpath.startswith('/user/hadoop/')):
+        print 'Unauthorized path: %(outpath)s' % locals()
+        return
+    run('hadoop fs -rm -r -f -skipTrash %(outpath)s &> /dev/null' % locals())
+    run('''cat <<EOF > /home/hadoop/demo/nlp.word2vec_draw.py
+# -*- encoding: utf-8 -*-
+from numpy import *
+from numpy.linalg import *
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib
+ 
+def PCA(data, dims_rescaled_data=2):
+    """
+    returns: data transformed in 2 dims/columns + regenerated original data
+    pass in: data as 2D NumPy array
+    """
+    import numpy as NP
+    from scipy import linalg as LA
+    m, n = data.shape
+    data -= data.mean(axis=0)
+    R = NP.cov(data, rowvar=False)
+    evals, evecs = LA.eigh(R)
+    idx = NP.argsort(evals)[::-1]
+    evecs = evecs[:,idx]
+    evals = evals[idx]
+    evecs = evecs[:, :dims_rescaled_data]
+    return NP.dot(evecs.T, data.T).T, evals, evecs
+     
+argmodelfile = '/hdfs' + '%(modelpath)s' + '/vectors.txt'
+argoutpath = '/hdfs' + '%(outpath)s'
+argtopn = int('%(topn)s')
+  
+fread = open(argmodelfile, 'r')
+wordvectors = {}
+for f in fread.readlines():
+    tok = f.split("\t")
+    w = tok[0].strip()
+    v = tok[1].strip().split(" ")
+    wordvectors[w] = [double(d) for d in v]
+  
+words = [wv[0] for wv in wordvectors.items()]
+vectors = [wv[1] for wv in wordvectors.items()]
+  
+rescaled , eigenvalues , eigenvectors = PCA(array(vectors), dims_rescaled_data=2)
+nnplist = []
+with open('/hdfs/user/hadoop/demo/nlp/hani-nnplist.txt', 'r') as fread:
+    nnplist = [s.split("\t")[0].strip() for s in fread.readlines()[:argtopn]]
+nnpindexes = [i for (i, w) in enumerate(words) if w.strip() in nnplist]
+showvecs = array([rescaled[i] for i in nnpindexes])
+showwords = [words[i] for i in nnpindexes]
+ 
+matplotlib.rc('font', family='UnDinaru')  # 한글 폰트 설정
+fig = plt.figure(figsize=(20,10))
+ax = fig.add_subplot(111)
+ax.plot(array(showvecs[:,0]), array(showvecs[:,1]), 'ro')
+for i in range(len(showvecs)):
+    ax.text(array(showvecs[i,0]), array(showvecs[i,1]), showwords[i].decode('utf-8'))
+fig.savefig(argoutpath)
+EOF''' % locals())
+    cmd = 'python2.7 /home/hadoop/demo/nlp.word2vec_draw.py'
     run(cmd)
