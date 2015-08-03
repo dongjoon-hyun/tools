1da73ee0feba141f00ecb6c20d97ba54c57893be
swlsw <soonwoong.lee@gmail.com>
2015-08-03 15:44:13 +0900

    fix send_photo to send local file

--- cli/tg.py
+++ cli/tg.py
@@ -32,12 +32,13 @@ def send_photo(photo_path, id=-36901391):
     fab tg.send_photo:https://telegram.org/img/t_logo.png
     """
     run('mkdir %s' % env.dir)
+    photo_path = 'open("' + photo_path + '", "rb")' if 'http' not in photo_path else '"' + photo_path + '"'
     with cd(env.dir):
         run('''cat <<EOF > tg_bot_request.py
 # -*- coding: utf-8 -*-
 import telegram
 bot = telegram.Bot(token='112152357:AAGBdO_TGBagwQCsoGWm4EcZNv1CRwhJ0JA')
-bot.sendPhoto(chat_id=%(id)s, photo='%(photo_path)s')
+bot.sendPhoto(chat_id=%(id)s, photo=%(photo_path)s)
 EOF''' % locals())
         cmd = 'python2.7 ./tg_bot_request.py 2>/dev/null'
         run(cmd)
