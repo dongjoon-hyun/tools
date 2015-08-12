ac6900d3cd68bd1d1ba62ebeeaf41ca521e73566
swlsw <soonwoong.lee@gmail.com>
2015-08-12 16:26:45 +0900

    fix deepdist to run on g98

--- cli/deepdist.py
+++ cli/deepdist.py
@@ -12,7 +12,6 @@ from fabric.api import *
 
 
 @task
-@hosts('50.1.100.101')
 def word2vec(inpath, positive, negative):
     """
     fab deepdist.word2vec:/sample/sample_enwiki.head,woman:king,man
@@ -26,7 +25,7 @@ from gensim.models.word2vec import Word2Vec
 from pyspark import SparkConf, SparkContext
 
 sc = SparkContext(appName="DeepDist word2vec")
-corpus = sc.textFile('hdfs://gnn-f02-01%(inpath)s').map(lambda s: s.split())
+corpus = sc.textFile('%(inpath)s').map(lambda s: s.split())
 
 def gradient(model, sentences):  # executes on workers
     syn0, syn1 = model.syn0.copy(), model.syn1.copy()
@@ -37,10 +36,10 @@ def descent(model, update):      # executes on master
     model.syn0 += update['syn0']
     model.syn1 += update['syn1']
 
-with DeepDist(Word2Vec(corpus.collect())) as dd:
+with DeepDist(Word2Vec(corpus.collect()), '50.1.100.98:5000') as dd:
     dd.train(corpus, gradient, descent)
     print dd.model.most_similar(positive=[%(positive)s], negative=[%(negative)s])
 EOF''' % locals())
-    # cmd = '/opt/spark/bin/spark-submit --master spark://50.1.100.98:7077 --driver-memory 4G --executor-memory 4G /home/hadoop/demo/deepdist.word2vec.py 2> /dev/null | tail -n 1'
-    cmd = '/opt/spark/bin/spark-submit --master local[8] --driver-memory 2G --executor-memory 4G /home/hadoop/demo/deepdist.word2vec.py 2> /dev/null | tail -n 1'
+    cmd = '/opt/spark/bin/spark-submit --master spark://50.1.100.98:7077 --driver-memory 4G --executor-memory 4G /home/hadoop/demo/deepdist.word2vec.py 2> /dev/null | tail -n 1'
+#     cmd = '/opt/spark/bin/spark-submit --master local[8] --driver-memory 2G --executor-memory 4G /home/hadoop/demo/deepdist.word2vec.py 2> /dev/null | tail -n 1'
     run(cmd)
