d9a644a628862c74e4695c9b55aa643064040405
Dongjoon Hyun <dongjoon@apache.org>
2015-08-19 13:04:08 +0900

    Indentation

--- cli/hive.py
+++ cli/hive.py
@@ -4,19 +4,21 @@
 Intelligence Platform CLI Fabric File
 """
 
-__author__    = 'Dongjoon Hyun (dongjoon@apache.org)'
-__license__   = 'Apache License'
-__version__   = '0.2'
+__author__ = 'Dongjoon Hyun (dongjoon@apache.org)'
+__license__ = 'Apache License'
+__version__ = '0.2'
 
 from fabric.api import *
 
 @task
 def sql(sql='show tables'):
-	"""
-	fab hive.sql:'select count(*) from data.news'
-	"""
-	run('''cat <<EOF > /home/hadoop/demo/hive.sql
+    """
+    fab hive.sql:'select count(*) from data.news'
+    """
+    run('mkdir %s' % env.dir)
+    with cd(env.dir):
+        run('''cat <<EOF > sql.hive
 %(sql)s
 EOF''' % locals())
-	cmd = '/usr/bin/hive -S --hiveconf mapred.map.tasks=300 --hiveconf hive.merge.mapfiles=false -f /home/hadoop/demo/hive.sql 2> /dev/null'
-	run(cmd)
+        cmd = '/usr/bin/hive -S --hiveconf mapred.map.tasks=300 --hiveconf hive.merge.mapfiles=false -f sql.hive 2> /dev/null'
+        run(cmd)
