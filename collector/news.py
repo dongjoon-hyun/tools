ede203b52f20498bd350473f3c39a4bae0f772cb
Dongjoon Hyun <dongjoon@apache.org>
2016-01-29 17:07:30 -0800

    Update news collector.

--- collector/news.py
+++ collector/news.py
@@ -22,11 +22,6 @@
 News Crawler
 """
 
-__author__ = 'Dongjoon Hyun (dongjoon@apache.org)'
-__copyright__ = 'Copyright (c) 2015-2016'
-__license__ = 'Apache License'
-__version__ = '0.1'
-
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -34,17 +29,28 @@ sys.setdefaultencoding('utf8')
 import os
 import time
 from datetime import date
-import getopt
+import StringIO
 import sqlite3
 import urllib2
 import tldextract
 import feedparser
 from bs4 import BeautifulSoup
-import StringIO
 from twisted.protocols.ftp import FileNotFoundError
+import gflags
+
+__author__ = 'Dongjoon Hyun (dongjoon@apache.org)'
+__copyright__ = 'Copyright (c) 2015-2016'
+__license__ = 'Apache License'
+__version__ = '0.1'
+
+FLAGS = gflags.FLAGS
+gflags.DEFINE_string('crawl', None, 'Feed-list file name.', short_name='c')
+gflags.DEFINE_boolean('parse', None, 'Parse.', short_name='p')
+gflags.DEFINE_string('feed', None, 'Day and domain.', short_name='f')
+gflags.DEFINE_boolean('debug', False, 'produces debugging output')
 
 
-class News:
+class News(object):
     """
     News
     """
@@ -53,20 +59,18 @@ class News:
         pass
 
     @staticmethod
-    def crawl(feed_list_file, verbose=False):
+    def crawl(feed_list_file):
         """
         Read feed list file and call crawl_feed for each feed
         """
-        f = open(feed_list_file, 'r')
-        feeds = f.readlines()
-        f.close()    
-        for feed in feeds:
-            url = feed.split()[-1]
-            News.crawl_feed(url, verbose)
-            time.sleep(1)
+        with open(feed_list_file, 'r') as feeds:
+            for feed in feeds.readlines():
+                url = feed.split()[-1]
+                News.crawl_feed(url)
+                time.sleep(1)
 
     @staticmethod
-    def crawl_feed(url, verbose):
+    def crawl_feed(url):
         """
         Crawl a single feed
         """
@@ -75,30 +79,35 @@ class News:
         try:
             os.mkdir(day)
         except OSError:
-            print("Path exists: " + day)
+            print "Path exists: " + day
         try:
             print url
-            d = feedparser.parse(url)
+            data = feedparser.parse(url)
         except:
             print 'Error', url
             return
 
+        print day, domain
         with sqlite3.connect("%(day)s/%(domain)s.db" % locals()) as conn:
-            c = conn.cursor()
-            c.execute('CREATE TABLE IF NOT EXISTS news (' +
-                      'ID INTEGER PRIMARY KEY AUTOINCREMENT, ' +
-                      'url TEXT UNIQUE, ' +
-                      'content TEXT)')
+            cur = conn.cursor()
+            cur.execute('CREATE TABLE IF NOT EXISTS news ('
+                        'ID INTEGER PRIMARY KEY AUTOINCREMENT, '
+                        'url TEXT UNIQUE, '
+                        'content TEXT)')
             try:
                 rows = [(u'%s' % e.link,
-                         u'%s' % unicode(' '.join(BeautifulSoup(e.description).get_text().split())).encode('utf8'))
-                        for e in d.entries]
-                c.executemany("INSERT OR IGNORE INTO news(url,content) VALUES(?,?)", rows)
+                         u'%s' % unicode(' '.join(BeautifulSoup(e.description, "html.parser")
+                                                  .get_text().split())).encode('utf8'))
+                        for e in data.entries]
+                cur.executemany("INSERT OR IGNORE INTO news(url,content) VALUES(?,?)", rows)
             except:
                 pass
             conn.commit()
 
-    def parse_feed(self, day_and_domain, verbose=False):
+    def parse_feed(self, day_and_domain):
+        """
+        Parse feed.
+        """
         try:
             # ex) day_and_domain = '2015-01-23_etnews'
             day = day_and_domain[:10]
@@ -110,69 +119,74 @@ class News:
             print 'Invalid day and domain: ', db_path
             return
         print day, domain
-        
+
         rows = []
         with sqlite3.connect("%(day)s/%(domain)s.db" % locals()) as conn:
-            c = conn.cursor()
-            c.execute('SELECT url FROM news')
-            rows = c.fetchall()
-            
+            cur = conn.cursor()
+            cur.execute('SELECT url FROM news')
+            rows = cur.fetchall()
+
         for index, row in enumerate(rows, start=1):
             try:
                 url = row[0]
                 print '%s : (%s/%s)' % (url, index, len(rows))
                 response = urllib2.urlopen(url)
                 html = response.read()
-                self.parse(StringIO.StringIO(html), verbose)
+                self.parse(StringIO.StringIO(html))
                 time.sleep(1)
-            except Exception as e:
-                print e
-                pass
+            except Exception as ex:
+                print ex
         print "TOTAL: %s pages" % len(rows)
 
-    def parse(self, input=sys.stdin, verbose=False):
+    def parse(self, ins=sys.stdin):
         """
         Parse news article with opengraph protocol
         """
-        html = ''.join(input.readlines())
-        soup = BeautifulSoup(html)
+        html = ''.join(ins.readlines())
+        soup = BeautifulSoup(html, "html.parser")
         url = self.get_url(soup)
         domain = tldextract.extract(url).domain
         title = self.get_title(soup)
         article = self.get_article(soup)
-        if verbose:
-            print url
+        if FLAGS.debug:
+            print domain, url
             print title
             print article
-        with sqlite3.connect("opengraph/%(domain)s.db" % locals()) as conn:
-            c = conn.cursor()
-            c.execute('CREATE TABLE IF NOT EXISTS news (' +
-                      'ID INTEGER PRIMARY KEY AUTOINCREMENT, ' +
-                      'url TEXT UNIQUE, ' +
-                      'title TEXT, ' +
-                      'article TEXT)')
-            c.execute('INSERT OR IGNORE INTO news(url,title,article) VALUES(?,?,?)', (url, title, article))
+        with sqlite3.connect("%(domain)s.db" % locals()) as conn:
+            cur = conn.cursor()
+            cur.execute('CREATE TABLE IF NOT EXISTS news (' +
+                        'ID INTEGER PRIMARY KEY AUTOINCREMENT, ' +
+                        'url TEXT UNIQUE, ' +
+                        'title TEXT, ' +
+                        'article TEXT)')
+            cur.execute('INSERT OR IGNORE INTO news(url,title,article) VALUES(?,?,?)',
+                        (url, title, article))
             conn.commit()
-        
+
     @staticmethod
     def get_url(soup):
+        """Get open-graph permanent url."""
         url = soup.find('meta', {'property': 'og:url'}).get('content')
         return url
 
     @staticmethod
     def get_title(soup):
+        """Get title of article"""
         title = soup.find('meta', {'property': 'og:title'}).get('content')
         if title is None:
             title = soup.title.get_text()
         return title
-        
+
     @staticmethod
     def is_empty(articles):
-        return 0 == len(articles) or \
-               0 == len(''.join([' '.join(a.get_text().split()) for a in articles if len(a.text.strip()) > 0]))
+        """Check if the set of article is empty."""
+        return len(articles) == 0 or len(''.join([' '.join(a.get_text().split())
+                                                  for a in articles
+                                                  if len(a.text.strip()) > 0])) == 0
 
     def get_article(self, soup):
-        [s.extract() for s in soup(['script','iframe','style'])]
+        """Get article."""
+        [s.extract() for s in soup(['script', 'iframe', 'style'])]
         articles = soup.findAll('div', {'class': 'article'})
         if self.is_empty(articles):  # joins
             articles = soup.findAll('div', {'class': 'article_content'})
@@ -188,27 +202,26 @@ class News:
             articles = soup.findAll('div', {'id': 'artText'})
         if self.is_empty(articles):  # chosun
             articles = soup.findAll('div', {'id': 'news_body_id'})
-        return ''.join([' '.join(a.get_text().split()) for a in articles if len(a.text.strip())>0])
+        return ''.join([' '.join(a.get_text().split())
+                        for a in articles
+                        if len(a.text.strip()) > 0])
 
 
-if __name__ == '__main__':
+def main(argv):
+    """Main."""
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hvc:pf:", ["help", "verbose", "crawl", "parse", "feed"])
-    except getopt.GetoptError as err:
-        sys.exit(0)
+        FLAGS(argv)
+    except gflags.FlagsError, ex:
+        print '%s' % ex
+        sys.exit(1)
 
     news = News()
-    isVerbose = False
-    for o, a in opts:
-        if o in ("-h", "--help"):
-            sys.exit()
-        elif o in ("-v", "--verbose"):
-            isVerbose = True
+    if FLAGS.crawl:
+        news.crawl(feed_list_file=FLAGS.crawl)
+    if FLAGS.parse:
+        news.parse()
+    if FLAGS.feed:
+        news.parse_feed(day_and_domain=FLAGS.feed)
 
-    for o, a in opts:
-        if o in ("-c", '--crawl'):
-            news.crawl(feed_list_file=a, verbose=isVerbose)
-        elif o in ("-p", '--parse'):
-            news.parse(verbose=isVerbose)
-        elif o in ("-f", '--feed'):
-            news.parse_feed(day_and_domain=a, verbose=isVerbose)
+if __name__ == '__main__':
+    main(sys.argv)
